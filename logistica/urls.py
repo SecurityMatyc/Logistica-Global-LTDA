5c988b646aa0b81fe84806901c654916f235ddf0
Matias Gajardo Castillo <mathi25gaj@gmail.com>
2025-11-15 22:53:13 -0300

    commit.commit

--- logistica/urls.py
+++ logistica/urls.py
@@ -17,9 +17,35 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+# Configuración de la documentación Swagger
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Logística Global",
+        default_version='v1',
+        description="Documentación de la API del sistema de logística.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contacto@logistica.cl"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# Configuración de las URL
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('admin/', admin.site.urls),
     path('', include('transporte.urls')),
+    # Documentación Swagger UI
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # Documentación Redoc
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Esquemas OpenAPI
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger.yaml/', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
 ]
